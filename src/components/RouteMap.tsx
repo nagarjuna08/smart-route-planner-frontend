45586de38de0023d60f1a59411153212b9c4b551
nagarjuna08 <nagarjunakakunuri449@gmail.com>
2026-05-31 18:46:23 +0530

    Made UI responsive

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -12,6 +12,7 @@ import L from "leaflet";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { useMap } from "react-leaflet";
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 
